ee93abb43f659c8561b25022865c897ab656e3be
Lloyd Emelle <home@emelle.me>
2011-11-13 18:18:13 -0500

    Update to SF Importer

--- _config.php
+++ _config.php
@@ -1,3 +1,16 @@
 <?php
 
 //Configuration Here
+
+/* URL Rules for Map View */
+Director::addRules(50, array('sfxml/$Action/$ID' => 'SFImporter_Controller'));
+global $sfdatabaseConfig;
+$sfdatabaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'dev-toby',
+	"database" => 'sftest',
+	"path" => '',
+);
+Director::set_environment_type("dev");
--- code/SFImporter.php
+++ code/SFImporter.php
@@ -1,5 +1,23 @@
 <?php
 
-class SFImporter extends Page{
-
+class SFImporter_Controller extends Controller {
+	private $sfdatabaseconfig = array(
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'dev-toby',
+	"database" => 'sftest'
+	);
+    public function index($arguments) {
+    
+    	global $databaseConfig,$sfdatabaseConfig;
+    	
+		DB::connect($sfdatabaseConfig);
+		$query = new SQLQuery("*", "wp_sftags");
+    	$result = $query->execute();
+    	//var_dump($result);
+    	//exit;
+    	//Connect to simpleforum database
+    	header('Content-Type: text/xml;encoding=utf-8');
+        return $this->renderWith('SFImporter');
+    }
 }
